3c06203edeb625914fe8dc37c11d57f3d5b31afb
Slaymd <darius.martin@epitech.eu>
2018-04-05 16:18:01 +0200

    (Entities) removed old function

--- src/ennemy/ennemy_generation.c
+++ src/ennemy/ennemy_generation.c
@@ -10,13 +10,13 @@
 entity_infos_t get_entity_infos(entity_type type)
 {
 	entity_infos_t ents_infos[] = {{OSTRICH,{0,0,0,0},1,1,1,1,ENT_OSTRICH},
-		{UNKNOWN,{0,0,0,0},0,0,0}};
+		{UNKNOWN,{0,0,0,0},0,0,0,0,""}};
 
 	for (int i = 0; ents_infos[i].type != UNKNOWN; i++) {
 		if (ents_infos[i].type == OSTRICH)
 			return (ents_infos[i]);
 	}
-	return ((entity_infos_t){UNKNOWN,0,0,0});
+	return ((entity_infos_t){UNKNOWN,{0,0,0,0},0,0,0,0,""});
 }
 
 entity_t *create_entity(entity_type type)
@@ -40,14 +40,15 @@ entity_t *create_entity(entity_type type)
 
 entity_t *add_ennemy_class_1(const char *path_sprite, sfIntRect square)
 {
-	entity_t *new = (entity_t*)malloc(sizeof(entity_t));
+	return (NULL);
+	/*entity_t *new = (entity_t*)malloc(sizeof(entity_t));
 	sfVector2f size = {0,0};
 
 	if (new == NULL)
 		return (NULL);
 	for (int i = 0; i == 0; i = 1, size.x = square.width, size.y =
 	square.height, new->lvl = 0, new->type = 1, new->ennemy_range = 0,
-	new->stat = 1103101);
+	//new->stat = 1103101);
 	new->sprite = sfSprite_create();
 	new->texture = sfTexture_createFromFile(path_sprite, NULL);
 	new->square = square;
@@ -55,7 +56,7 @@ entity_t *add_ennemy_class_1(const char *path_sprite, sfIntRect square)
 	sfSprite_setTexture(new->sprite, new->texture, sfTrue);
 	sfSprite_setTextureRect(new->sprite, new->square);
 	sfSprite_setScale(new->sprite, (sfVector2f){0.6, 0.6});
-	return (new);
+	return (new);*/
 }
 
 void init_sprite(rpg_t *rpg)
