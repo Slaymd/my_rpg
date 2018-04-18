c3dba5096672bb940d60cf14f4c07f8ed14f9028
Dakiche <raphael2.dakiche@epitech.eu>
2018-04-18 18:15:03 +0200

    Song folder deleted

--- src/entity/combat.c
+++ src/entity/combat.c
@@ -13,22 +13,22 @@ void follow_ostrich(rpg_t *rpg, entity_t *ent, map_t *map)
 	int y = map->topleft_to_disp.y + (HEIGHT / TILE_SIZE / 2);
 	if (ent->pos.x < rpg->character->pos.x) {
 		ent->pos.x += 0.125;
-		my_printf("%s\n", "d");
+		// my_printf("%s\n", "d");
 	}
 	if (ent->pos.x > rpg->character->pos.x) {
-		my_printf("px = %f et ent = %f\n", ent->pos.x, rpg->character->pos.x);
+		// my_printf("px = %f et ent = %f\n", ent->pos.x, rpg->character->pos.x);
 		ent->pos.x -= 0.125;
-		my_printf("%s\n", "g");
+		// my_printf("%s\n", "g");
 	}
 	if (ent->pos.y > rpg->character->pos.y) {
-		my_printf("px = %f et ent = %f\n", ent->pos.y, rpg->character->pos.y);
+		// my_printf("px = %f et ent = %f\n", ent->pos.y, rpg->character->pos.y);
 		ent->pos.y -= 0.125;
-		my_printf("%s\n", "g");
+		// my_printf("%s\n", "g");
 	}
-	my_printf("y = %d et x  = %d\n", y, x);
+	// my_printf("y = %d et x  = %d\n", y, x);
 	if (ent->pos.y < rpg->character->pos.y) {
 		//ent->pos.y += 0.125;
-		my_printf("%s\n", "g");
+		// my_printf("%s\n", "g");
 	}
 }
 
