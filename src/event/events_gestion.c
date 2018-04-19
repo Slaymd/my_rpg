72be78c556c786b5548a6e097bfd02de7cd29f69
Slaymd <darius.martin@epitech.eu>
2018-04-19 21:16:10 +0200

    (Collisions) added collisions with water and stone

--- src/event/events_gestion.c
+++ src/event/events_gestion.c
@@ -5,23 +5,75 @@
 ** event
 */
 
-#include "rpg.h"
+#include "../../include/rpg.h"
+
+int map_move_left(map_t *map)
+{
+	float player_x = map->topleft_to_disp.x + (WIDTH / TILE_SIZE / 2);
+	float player_y = map->topleft_to_disp.y+1 + (HEIGHT / TILE_SIZE / 2);
+	pos_t player_pos = {player_x - MOVE_SPEED, player_y, 0};
+	int canmove = can_move_here(map, player_pos);
+
+	printf("--> %d\n", canmove);
+	if (!canmove)
+		map->topleft_to_disp.x -= MOVE_SPEED;
+	return (0);
+}
+
+int map_move_right(map_t *map)
+{
+	float player_x = map->topleft_to_disp.x + (WIDTH / TILE_SIZE / 2);
+	float player_y = map->topleft_to_disp.y+1 + (HEIGHT / TILE_SIZE / 2);
+	pos_t player_pos = {player_x + MOVE_SPEED, player_y, 0};
+	int canmove = can_move_here(map, player_pos);
+
+	printf("--> %d\n", canmove);
+	if (!canmove)
+		map->topleft_to_disp.x += MOVE_SPEED;
+	return (0);
+}
+
+int map_move_up(map_t *map)
+{
+	float player_x = map->topleft_to_disp.x + (WIDTH / TILE_SIZE / 2);
+	float player_y = map->topleft_to_disp.y+1 + (HEIGHT / TILE_SIZE / 2);
+	pos_t player_pos = {player_x, player_y - MOVE_SPEED, 0};
+	int canmove = can_move_here(map, player_pos);
+
+	printf("--> %d\n", canmove);
+	if (!canmove)
+		map->topleft_to_disp.y -= MOVE_SPEED;
+	return (0);
+}
+
+int map_move_down(map_t *map)
+{
+	float player_x = map->topleft_to_disp.x + (WIDTH / TILE_SIZE / 2);
+	float player_y = map->topleft_to_disp.y+1 + (HEIGHT / TILE_SIZE / 2);
+	pos_t player_pos = {player_x, player_y + MOVE_SPEED, 0};
+	int canmove = can_move_here(map, player_pos);
+
+	printf("--> %d\n", canmove);
+	if (!canmove)
+		map->topleft_to_disp.y += MOVE_SPEED;
+	return (0);
+}
 
 int map_move(sfEvent event, map_t *map)
 {
 	if (event.type == sfEvtKeyPressed) {
 		switch (event.key.code) {
 		case sfKeyLeft:
-			map->topleft_to_disp.x -= MOVE_SPEED;
+			map_move_left(map);
 			return (1);
 		case sfKeyRight:
-			map->topleft_to_disp.x += MOVE_SPEED;
+			map_move_right(map);
 			return (1);
 		case sfKeyUp:
-			map->topleft_to_disp.y -= MOVE_SPEED;
+			map_move_up(map);
 			return (1);
 		case sfKeyDown:
-			map->topleft_to_disp.y += MOVE_SPEED;
+			map_move_down(map);
 			return (1);
 		default:
 			return (0);
--- src/map/collision/tiles_collision.c
+++ src/map/collision/tiles_collision.c
@@ -11,9 +11,11 @@ int	can_move_here(map_t *map, pos_t pos)
 {
 	tile_t *tile = get_tile_at(map, pos);
 
+	printf("(%f, %f)\n", pos.x, pos.y);
 	if (tile == NULL)
 		return (-1);
-	if (tile->texture_id > 2 && tile->texture_id < 4)
+	printf("TXTRE ID : %d\n", tile->texture_id);
+	if (tile->texture_id <= 2 || tile->texture_id > 4)
 		return (1);
 	return (0);
 }
