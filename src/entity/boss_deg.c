3f691938e7d5d4a43867c376eb6f126d86f2daa6
Maiguier <maxime.aiguier@epitech.eu>
2018-05-09 10:12:51 +0200

    (Upraded od boss colision

--- src/entity/boss_deg.c
+++ src/entity/boss_deg.c
@@ -19,20 +19,45 @@ int detect_attack_char(entity_t *ent, map_t *map)
 	return (0);
 }
 
+void detect_blocked_x(entity_t *ent, map_t *map, int block_x)
+{
+	if (block_x == 2 && can_move_here(map, (pos_t)
+	{ent->pos.x, ent->pos.y + 0.56, ent->pos.z}) == 1)
+		ent->pos.y += 0.56;
+	else if (block_x == 2 && can_move_here(map, (pos_t)
+	{ent->pos.x, ent->pos.y - 0.56, ent->pos.z}) == 1)
+		ent->pos.y -= 0.56;
+}
+
+void detect_blocked_y(entity_t *ent, map_t *map, int block_x, int block_y)
+{
+	if (block_y == 2 && block_x >= 1 && can_move_here(map, (pos_t)
+		{ent->pos.x + 0.12, ent->pos.y, ent->pos.z}) == 1)
+		ent->pos.x += 0.12;
+	else if (block_y == 2 && block_x >= 1 && can_move_here(map, (pos_t)
+		{ent->pos.x - 0.12, ent->pos.y, ent->pos.z}) == 1)
+		ent->pos.x -= 0.12;
+	detect_blocked_x(ent, map, block_x);
+
+}
+
 int run_char(entity_t *ent, map_t *map, int xx, int yy)
 {
 	int pos = 0;
+	int block_x = 0;
+	int block_y = 0;
 
 	ent->pos.x <= xx + 0.3 && can_move_here(map,
 	(pos_t){ent->pos.x + 0.12, ent->pos.y, ent->pos.z}) == 1 ?
-	ent->pos.x += 0.12, ent->mirror = 3 : 0;
+	ent->pos.x += 0.12, ent->mirror = 3 : block_x++;
 	ent->pos.x >= xx + 0.3 && can_move_here(map, (pos_t){ent->pos.x
 	- 0.12, ent->pos.y, ent->pos.z}) == 1 ? ent->pos.x -= 0.12,
-	ent->mirror = 4 : 0;
+	ent->mirror = 4 : block_x++;
 	pos = xx;
 	ent->pos.y >= yy && can_move_here(map, (pos_t){ent->pos.x,
-	ent->pos.y - 0.16, ent->pos.z}) == 1 ? ent->pos.y -= 0.16 : 0;
+	ent->pos.y - 0.16, ent->pos.z}) == 1 ? ent->pos.y -= 0.16 : block_y++;
 	ent->pos.y <= yy && can_move_here(map, (pos_t){ent->pos.x,
-	ent->pos.y + 0.16, ent->pos.z}) == 1 ? ent->pos.y += 0.16 : 0;
+	ent->pos.y + 0.16, ent->pos.z}) == 1 ? ent->pos.y += 0.16 : block_y++;
+	detect_blocked_y(ent, map, block_x, block_y);
 	return (pos);
 }
