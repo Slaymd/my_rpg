d467ac7c0d73e1787ae3dc8022dff124601ee7f4
benjamin <benjamin.renaud@epitech.eu>
2018-05-13 21:46:36 +0200

    norme and init stat

--- src/inventory/drop_item.c
+++ src/inventory/drop_item.c
@@ -11,14 +11,16 @@ void fill_att(inv_t *inv)
 {
 	int cpt = 0;
 
-	for (cpt = 2; inv->slot[cpt] != NULL; cpt++)
+	for (cpt = 2; inv->slot[cpt] != NULL; cpt++) {
 		if (inv->slot[cpt]->in_body == 0 && inv->slot[cpt]->status == 0)
 			break;
 		if (cpt > 13)
 			return;
+	}
 	inv->slot[cpt]->titem = sfTexture_createFromFile
 	("assets/inventory/att.png", NULL);
-	sfSprite_setTexture(inv->slot[cpt]->sitem, inv->slot[cpt]->titem, sfTrue);
+	sfSprite_setTexture
+	(inv->slot[cpt]->sitem, inv->slot[cpt]->titem, sfTrue);
 	inv->slot[cpt]->can_e = 2;
 	inv->slot[cpt]->status = 1;
 	inv->slot[cpt]->attack = 5;
@@ -29,14 +31,16 @@ void fill_att2(inv_t *inv)
 {
 	int cpt = 0;
 
-	for (cpt = 2; inv->slot[cpt] != NULL; cpt++)
+	for (cpt = 2; inv->slot[cpt] != NULL; cpt++) {
 		if (inv->slot[cpt]->in_body == 0 && inv->slot[cpt]->status == 0)
 			break;
 		if (cpt > 13)
 			return;
+	}
 	inv->slot[cpt]->titem = sfTexture_createFromFile
 	("assets/inventory/att2.png", NULL);
-	sfSprite_setTexture(inv->slot[cpt]->sitem, inv->slot[cpt]->titem, sfTrue);
+	sfSprite_setTexture
+	(inv->slot[cpt]->sitem, inv->slot[cpt]->titem, sfTrue);
 	inv->slot[cpt]->can_e = 2;
 	inv->slot[cpt]->status = 1;
 	inv->slot[cpt]->attack = 20;
@@ -47,7 +51,7 @@ void fill_life_potion(inv_t *inv)
 {
 	int cpt = 0;
 
-	for (cpt = 2; inv->slot[cpt] != NULL; cpt++)
+	for (cpt = 2; inv->slot[cpt] != NULL; cpt++) {
 		if ((inv->slot[cpt]->in_body == 0 &&
 			inv->slot[cpt]->status == 0) ||
 			(inv->slot[cpt]->status == 1 &&
@@ -55,9 +59,11 @@ void fill_life_potion(inv_t *inv)
 			break;
 		if (cpt > 13)
 			return;
+	}
 	inv->slot[cpt]->titem = sfTexture_createFromFile
 	("assets/inventory/life_potion.png", NULL);
-	sfSprite_setTexture(inv->slot[cpt]->sitem, inv->slot[cpt]->titem, sfTrue);
+	sfSprite_setTexture
+	(inv->slot[cpt]->sitem, inv->slot[cpt]->titem, sfTrue);
 	inv->slot[cpt]->can_e = 0;
 	inv->slot[cpt]->status = 1;
 	inv->slot[cpt]->conso = 1;
@@ -68,7 +74,7 @@ void fill_mana_potion(inv_t *inv)
 {
 	int cpt = 0;
 
-	for (cpt = 2; inv->slot[cpt] != NULL; cpt++)
+	for (cpt = 2; inv->slot[cpt] != NULL; cpt++) {
 		if ((inv->slot[cpt]->in_body == 0 &&
 			inv->slot[cpt]->status == 0) ||
 			(inv->slot[cpt]->status == 1 &&
@@ -76,11 +82,36 @@ void fill_mana_potion(inv_t *inv)
 			break;
 		if (cpt > 13)
 			return;
+	}
 	inv->slot[cpt]->titem = sfTexture_createFromFile
 	("assets/inventory/mana_potion.png", NULL);
-	sfSprite_setTexture(inv->slot[cpt]->sitem, inv->slot[cpt]->titem, sfTrue);
+	sfSprite_setTexture
+	(inv->slot[cpt]->sitem, inv->slot[cpt]->titem, sfTrue);
 	inv->slot[cpt]->can_e = 0;
 	inv->slot[cpt]->status = 1;
 	inv->slot[cpt]->conso = 2;
+	inv->slot[cpt]->stack += 1;
+}
+
+void fill_popo(inv_t *inv)
+{
+	int cpt = 0;
+
+	for (cpt = 2; inv->slot[cpt] != NULL; cpt++) {
+		if ((inv->slot[cpt]->in_body == 0 &&
+			inv->slot[cpt]->status == 0) ||
+			(inv->slot[cpt]->status == 1 &&
+			inv->slot[cpt]->stack > 0))
+			break;
+		if (cpt > 13)
+			return;
+	}
+	inv->slot[cpt]->titem = sfTexture_createFromFile
+	("assets/inventory/popo.png", NULL);
+	sfSprite_setTexture
+	(inv->slot[cpt]->sitem, inv->slot[cpt]->titem, sfTrue);
+	inv->slot[cpt]->can_e = 0;
+	inv->slot[cpt]->status = 1;
+	inv->slot[cpt]->conso = 3;
 	inv->slot[cpt]->stack += 1;
 }
